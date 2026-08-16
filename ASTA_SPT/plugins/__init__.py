2e96d13b4c387a446d76d956cc3604b8244baa66
zxcodxe <saifilaibaa33@gmail.com>
2026-08-16 20:28:09 +0530

    Add files via upload

---
+++ ASTA_SPT/plugins/__init__.py
@@ -0,0 +1,35 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @ixasta1
+# =======================================================
+
+import glob
+from os.path import dirname, isfile
+
+
+def __list_all_modules():
+    work_dir = dirname(__file__)
+    mod_paths = glob.glob(work_dir + "/*/*.py")
+
+    all_modules = [
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 😎
+
+# 🧑‍💻 Developer : t.me/ixasta1
+# 🔗 Source link : GitHub.com/7808531413/Asta-MusicV2
+# 📢 Telegram channel : t.me/ixasta1
+# =======================================================

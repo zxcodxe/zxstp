64b275ff2fe4ea193f55e19911ed08a54b80fa73
zxcodxe <saifilaibaa33@gmail.com>
2026-08-16 20:32:21 +0530

    Add files via upload

---
+++ ASTA_SPT/utils/decorators/__init__.py
@@ -0,0 +1,18 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @ixasta1
+# =======================================================
+
+from .admins import *
+from .language import *
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 😎
+
+# 🧑‍💻 Developer : t.me/ixasta1
+# 🔗 Source link : GitHub.com/7808531413/Asta-MusicV2
+# 📢 Telegram channel : t.me/ixasta1
+# =======================================================

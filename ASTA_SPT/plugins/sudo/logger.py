8b154a1877590da9861d93038fdb45a42d910a4e
zxcodxe <saifilaibaa33@gmail.com>
2026-08-16 22:12:22 +0530

    Add files via upload

---
+++ ASTA_SPT/plugins/sudo/logger.py
@@ -0,0 +1,46 @@
+# =======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 🚀
+
+# This source code is under MIT License 📜 Unauthorized forking, importing, or using this code without giving proper credit will result in legal action ⚠️
+ 
+# 📩 DM for permission : @ixasta1
+# =======================================================
+
+from pyrogram import filters
+
+from ASTA_SPT import app
+from ASTA_SPT.misc import SUDOERS
+from ASTA_SPT.utils.database import add_off, add_on
+from ASTA_SPT.utils.decorators.language import language
+
+
+@app.on_message(filters.command(["logger"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state == "enable":
+        await add_on(2)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(2)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
+
+
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    await message.reply_document("cookies/logs.csv")
+    await message.reply_text("Please check given file to cookies file choosing logs...")
+
+# ======================================================
+# ©️ 2025-26 All Rights Reserved by ASTA SPT (7808531413) 😎
+
+# 🧑‍💻 Developer : t.me/ixasta1
+# 🔗 Source link : GitHub.com/7808531413/Asta-MusicV2
+# 📢 Telegram channel : t.me/ixasta1
+# =======================================================
